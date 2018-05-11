b13b9a22e4def1c5c32e3a266c480bfba9a23919
Angel Sanadinov <sndnv@users.noreply.github.com>
2018-05-11 23:37:38 +0200

    [scala] Finish basic service implementation

--- scala/src/main/scala/nonamedb/api/Service.scala
+++ scala/src/main/scala/nonamedb/api/Service.scala
@@ -15,18 +15,26 @@ class Service(engine: Engine)(implicit system: ActorSystem) {
   private implicit val materializer: ActorMaterializer = ActorMaterializer()
   private implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
-  def start(): Future[Http.ServerBinding] = Http().bindAndHandle(routes, "localhost", 8080)
+  def start(bindAddress: String, bindPort: Int): Future[Http.ServerBinding] =
+    Http().bindAndHandle(routes, bindAddress, bindPort)
 
   val routes: Route =
     path(Segment) { id =>
-      get {
-        onComplete(engine.get(id)) { result =>
-          result.toOption.flatten match {
-            case Some(data) => complete(HttpResponse(entity = HttpEntity(MediaTypes.`application/octet-stream`, data)))
-            case None       => reject
+      concat(
+        get {
+          onComplete(engine.get(id)) { result =>
+            result.toOption.flatten match {
+              case Some(data) =>
+                complete(
+                  HttpResponse(
+                    entity = HttpEntity(MediaTypes.`application/octet-stream`, data)
+                  )
+                )
+              case None =>
+                reject
+            }
           }
-        }
-      } ~
+        },
         (post | put) {
           extractDataBytes { dataStream =>
             complete {
@@ -40,11 +48,12 @@ class Service(engine: Engine)(implicit system: ActorSystem) {
                 }
             }
           }
-        } ~
+        },
         delete {
           complete {
             engine.put(id, Array.emptyByteArray)
           }
         }
+      )
     }
 }
--- scala/src/test/scala/nonamedb/test/specs/unit/api/ServiceSpec.scala
+++ scala/src/test/scala/nonamedb/test/specs/unit/api/ServiceSpec.scala
@@ -1,9 +1,11 @@
 package nonamedb.test.specs.unit.api
 
+import akka.http.javadsl.model.RequestEntity
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import akka.util.Timeout
+import akka.util.{ByteString, Timeout}
 import nonamedb.api.Service
+import nonamedb.storage.Value
 import nonamedb.storage.engines.memory.MemoryEngine
 import nonamedb.test.specs.unit.UnitSpec
 import org.scalatest.Outcome
@@ -21,37 +23,55 @@ class ServiceSpec extends UnitSpec with ScalatestRouteTest {
   private val testEngine = new MemoryEngine()
   private val testService = new Service(testEngine)
 
+  private val testKey = "some-key"
+  private val testValue = "some value".getBytes
+  private val updatedTestValue = "some updated value".getBytes
+
   "A Service" should "reject invalid requests" in { _ =>
     Get() ~> testService.routes ~> check {
-      fail("Not Implemented", new NotImplementedError())
+      handled should be(false)
     }
 
     Get("/missing-data") ~> testService.routes ~> check {
-      fail("Not Implemented", new NotImplementedError())
+      handled should be(false)
     }
   }
 
   it should "successfully add data" in { _ =>
-    fail("Not Implemented", new NotImplementedError())
+    Post(s"/$testKey").withEntity(testValue) ~> testService.routes ~> check {
+      status should be(StatusCodes.OK)
+    }
   }
 
   it should "successfully retrieve new data" in { _ =>
-    fail("Not Implemented", new NotImplementedError())
+    Get(s"/$testKey") ~> testService.routes ~> check {
+      status should be(StatusCodes.OK)
+      responseAs[Value] should be(testValue)
+    }
   }
 
   it should "successfully update data" in { _ =>
-    fail("Not Implemented", new NotImplementedError())
+    Put(s"/$testKey").withEntity(updatedTestValue) ~> testService.routes ~> check {
+      status should be(StatusCodes.OK)
+    }
   }
 
   it should "successfully retrieve updated data" in { _ =>
-    fail("Not Implemented", new NotImplementedError())
+    Get(s"/$testKey") ~> testService.routes ~> check {
+      status should be(StatusCodes.OK)
+      responseAs[Value] should be(updatedTestValue)
+    }
   }
 
   it should "successfully delete data" in { _ =>
-    fail("Not Implemented", new NotImplementedError())
+    Delete(s"/$testKey") ~> testService.routes ~> check {
+      status should be(StatusCodes.OK)
+    }
   }
 
   it should "fail to retrieve delete data" in { _ =>
-    fail("Not Implemented", new NotImplementedError())
+    Get(s"/$testKey") ~> testService.routes ~> check {
+      handled should be(false)
+    }
   }
 }
